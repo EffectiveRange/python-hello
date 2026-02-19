be47a61471a5122c5a7bc2446b67c9eee7fb8859
Attila Gombos <attila.gombos@effective-range.com>
2026-02-19 09:09:24 +0100

    SB-23 Add network interface option

--- examples/cameraServiceExample.py
+++ examples/cameraServiceExample.py
@@ -3,7 +3,7 @@ from uuid import uuid4
 from context_logger import get_logger, setup_logging
 
 from examples import setup_shutdown
-from hello import ServiceInfo, Hello, Group
+from hello import ServiceInfo, Hello, Group, GroupUrl
 
 setup_logging('hello')
 
@@ -13,8 +13,11 @@ log = get_logger('CameraService')
 def main() -> None:
     shutdown_event = setup_shutdown()
 
+    # Define the group URL optionally specifying the network interface
+    url = GroupUrl(address='239.0.1.1', port=5555, interface='wlan0')
+
     # Define the group to advertise the camera service on
-    group = Group(name='effective-range/sniper', url='udp://239.0.1.1:5555')
+    group = Group.create(name='effective-range/sniper', url=url)
 
     # Define the service information for the camera
     hostname = 'er-sniper-camera-1'
